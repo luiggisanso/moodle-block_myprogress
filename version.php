10948a39646f1fe1b22f1e140b11bcdf4a34901d
Luiggi Sansonetti <moodle@luiggisansonetti.fr>
2024-07-11 15:46:05 +0200

    Update version.php

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->requires = 2011120511;
 $plugin->version = 2024042400;
-$plugin->release = '1.0.6';
+$plugin->release = '1.0.7';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'block_myprogress';
 $plugin->supported = [401, 404];
